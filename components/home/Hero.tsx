b2081b52e46daa34b37336602935934164a6fde2
Cosmin Sas <cosmindariussas@gmail.com>
2025-01-17 16:33:56 +0000

    Created the Hero, and Hero Carousel

--- components/home/Hero.tsx
+++ components/home/Hero.tsx
@@ -1,10 +1,24 @@
 import React from 'react'
+import HeroCarousel from './HeroCarousel'
+import { Button } from '../ui/button'
+import Link from 'next/link'
 
 function Hero() {
   return (
-    <h1>
-      Hero
-    </h1>
+    <section className='grid grid-cols-1 lg:grid-cols-2 gap-24 items-center'>
+      <div>
+        <h1 className='max-w-xl font-bold text-4xl tracking-tight sm:text-6xl'>
+          We are changing the way people shop
+        </h1>
+        <p className='mt-8 max-w-xl text-lg leading-8 text-muted-foreground'>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum itaque porro eius vel a fugiat ipsa deserunt consectetur accusantium laudantium!
+        </p>
+        <Button asChild size='lg' className='mt-10'>
+          <Link href='/products'>Our products</Link>
+        </Button>
+      </div>
+        <HeroCarousel/>
+    </section>
   )
 }
 
