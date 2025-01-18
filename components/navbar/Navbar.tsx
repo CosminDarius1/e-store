d93bb529939bbbfe3c597aa46926fe62d8013e7b
Cosmin Sas <cosmindariussas@gmail.com>
2025-01-18 17:05:11 +0000

    NavSearch working, user can search for specific products

--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -5,6 +5,7 @@ import NavSearch from './NavSearch'
 import CartButton from './CartButton'
 import DarkMode from './DarkMode'
 import LinksDropdown from './LinksDropdown'
+import { Suspense } from 'react'
 
 function Navbar() {
   return (
@@ -12,7 +13,9 @@ function Navbar() {
     <nav className='border-b'>
       <Container className='flex flex-col sm:flex-row sm:justify-between sm:items-center flex-wrap py-8 gap-4'>
         <Logo/>
-        <NavSearch/>
+        <Suspense>
+          <NavSearch/>
+        </Suspense>
         <div className='flex gap-4 items-center'>
           <CartButton/>
           <DarkMode/>
