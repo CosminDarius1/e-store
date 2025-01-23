36547a9d4e69d3f0496cc4960cc4cd35db11285f
Cosmin Sas <cosmindariussas@gmail.com>
2025-01-23 21:45:12 +0000

    ok

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -7,7 +7,8 @@ import AddToCart from '@/components/single-product/AddToCart';
 import ProductRating from '@/components/single-product/ProductRating';
 
 
-async function SingleProductPage({ params }: { params: { id: string } }) {
+async function SingleProductPage(props: { params: Promise<{ id: string }> }) {
+  const params = await props.params;
   const product = await fetchSingleProduct(params.id);
   const { name, image, company, description, price } = product;
   const dollarsAmount = formatCurrency(price);
