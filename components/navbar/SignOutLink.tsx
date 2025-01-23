f2655866c73336d1ef1cb3580d4241d07a2b4579
Cosmin Sas <cosmindariussas@gmail.com>
2025-01-23 18:41:41 +0000

    did the setup for clerk authentication, signout link component and userIcon component done

--- components/navbar/SignOutLink.tsx
+++ components/navbar/SignOutLink.tsx
@@ -1,8 +1,22 @@
+import { useToast } from '@/hooks/use-toast'
+import { SignOutButton } from '@clerk/nextjs'
+import Link from 'next/link'
 import React from 'react'
+'use client'
 
 function SignOutLinks() {
+  const {toast} = useToast()
+
+const handleLogout = () => {
+  toast({description:'Logout Successfull'})
+}
+
   return (
-    <div>SignOutLinks</div>
+    <SignOutButton>
+      <Link href='/' className='w-full text-left' onClick={handleLogout}>
+      Logout
+      </Link>
+    </SignOutButton>
   )
 }
 
