5db0520ceafb3036b9265d88914ed89f1031f035
Cosmin Sas <cosmindariussas@gmail.com>
2025-01-23 21:21:32 +0000

    add auth provider

--- components/navbar/SignOutLink.tsx
+++ components/navbar/SignOutLink.tsx
@@ -1,14 +1,14 @@
+'use client'
 import { useToast } from '@/hooks/use-toast'
 import { SignOutButton } from '@clerk/nextjs'
 import Link from 'next/link'
 import React from 'react'
-'use client'
 
 function SignOutLinks() {
   const {toast} = useToast()
 
 const handleLogout = () => {
-  toast({description:'Logout Successfull'})
+  toast({description:'Logout Successful'})
 }
 
   return (
