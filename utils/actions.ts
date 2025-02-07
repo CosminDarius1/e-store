4dcdfd2f2a43e04f0c088f1591c21ecdcf616f08
Cosmin Sas <cosmindariussas@gmail.com>
2025-02-07 16:32:53 +0000

    Finished fetch admin page,finished Edit Product Page and Update Image Container, created Loading Table Container

--- utils/actions.ts
+++ utils/actions.ts
@@ -7,6 +7,7 @@ import { redirect } from 'next/navigation';
 import { imageSchema, productSchema, validateWithZodSchema } from './schemas';
 import { deleteImage, uploadImage } from './supabase';
 import { revalidatePath } from 'next/cache';
+import { tr } from '@faker-js/faker';
 
 const getAuthUser = async() => {
     const user  = await currentUser()
@@ -117,5 +118,65 @@ export const createProductAction = async (
         return {message: 'product removed'}
     } catch(error) {
         return renderError(error)
+    }
+}
+
+export const fetchAdminProductDetails = async(productId:string) => {
+    await getAdminUser()
+    const product = await db.product.findUnique({
+        where:{
+            id:productId,
+        },
+    });
+    if(!product) redirect('/admin/products');
+    return product;
+}
+
+
+export const updateProductAction = async(prevState:any, formData:FormData) => {
+
+await getAdminUser()
+try {
+    const productId = formData.get('id') as string
+    const rawData = Object.fromEntries(formData)
+    const validatedFields = validateWithZodSchema(productSchema, rawData)
+
+await db.product.update({
+    where:{
+        id:productId
+    },
+    data:{
+        ...validatedFields
+    },
+});
+    revalidatePath(`/admin/products/${productId}/edit`)
+    return { message:'Product updated successfully'}
+} catch (error) {
+    return renderError(error)
+}
+}
+export const updateProductImageAction = async(prevState:any, formData:FormData) => {
+
+    await getAuthUser()
+    try {
+        const image = formData.get('image') as File
+        const productId = formData.get('id') as string;
+        const oldImageUrl = formData.get('url') as string
+
+        const validatedFile = validateWithZodSchema(imageSchema,{image});
+        const fullPath = await uploadImage(validatedFile.image)
+        await deleteImage(oldImageUrl)
+        await db.product.update({
+            where:{
+                id:productId
+            },
+            data:{
+                image: fullPath,
+            },
+        });
+        revalidatePath(`/admin/products/${productId}/edit`)
+        return { message:'Product updated successfully'}
+    } catch (error) {
+        return renderError(error);
     }
 }
