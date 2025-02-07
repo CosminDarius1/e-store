bc60ddf8f8e6ce53aeba261e277b8ad86aa75b12
Cosmin Sas <cosmindariussas@gmail.com>
2025-02-07 17:08:32 +0000

    errors

--- components/products/ProductsGrid.tsx
+++ components/products/ProductsGrid.tsx
@@ -20,7 +20,7 @@ function ProductsGrid({ products }: { products: Product[] }) {
                   <div className='relative h-64 md:h-48 rounded overflow-hidden '>
                     <Image
                       src={image}
-                      alt={name}
+                      alt={name ?? ""}
                       fill
                       sizes='(max-width:768px) 100vw,(max-width:1200px) 50vw,33vw'
                       priority
--- components/products/ProductsList.tsx
+++ components/products/ProductsList.tsx
@@ -18,7 +18,7 @@ function ProductsList({products}:{products:Product[]}) {
                     <Card className='transform group-hover:shadow-xl transition-shadow duration-500'>
                         <CardContent className='p-8 gap-y-4 grid md:grid-cols-3'>
                             <div className='relative h-4 md:h-48 md:w-48'>
-                                <Image src={image} alt={name} fill sizes='(max-width:768px) 100vw, (max-width:1200px) 50vw, 33vw' priority className='w-full rounded object-cover'/>
+                                <Image src={image} alt={name ?? ""} fill sizes='(max-width:768px) 100vw, (max-width:1200px) 50vw, 33vw' priority className='w-full rounded object-cover'/>
                             </div>
                             <div>
                                 <h2 className='text-xl font-semibold capitalize'>{name}</h2>
