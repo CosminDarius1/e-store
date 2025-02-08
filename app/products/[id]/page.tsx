f95518063415e1813028dde4f8004886dba89a12
Cosmin Sas <cosmindariussas@gmail.com>
2025-02-08 18:44:37 +0000

    Installed Next-share library, created LinkedIn,X and Mail share buttons for individual products

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -5,6 +5,7 @@ import { formatCurrency } from '@/utils/format';
 import FavoriteToggleButton from '@/components/products/FavoriteToggleButton';
 import AddToCart from '@/components/single-product/AddToCart';
 import ProductRating from '@/components/single-product/ProductRating';
+import ShareButton from '@/components/single-product/ShareButton';
 
 
 async function SingleProductPage(props: { params: Promise<{ id: string }> }) {
@@ -29,7 +30,10 @@ async function SingleProductPage(props: { params: Promise<{ id: string }> }) {
         <div>
           <div className='flex gap-x-8 items-center'>
             <h1 className='capitalize text-3xl font-bold'>{name}</h1>
+            <div className='flex items-center gap-x-2'>
             <FavoriteToggleButton productId={params.id} />
+            <ShareButton name={product.name || ''} productId={params.id}/>
+            </div>
           </div>
           <ProductRating productId={params.id} />
           <h4 className='text-xl mt-2'>{company}</h4>
