7853f3804bdc48ba36feac806a5dfb5f4dd8e21a
Cosmin Sas <cosmindariussas@gmail.com>
2025-02-08 16:26:24 +0000

    Favorites page and functionality working

--- components/products/FavoriteToggleButton.tsx
+++ components/products/FavoriteToggleButton.tsx
@@ -1,10 +1,19 @@
-import { FaHeart } from 'react-icons/fa';
-import { Button } from '@/components/ui/button';
-function FavoriteToggleButton({ productId }: { productId: string }) {
+
+import { CardSignInButton } from '../form/Buttons';
+import { fetchFavoriteId } from '@/utils/actions';
+import FavoriteToggleForm from './FavoriteToggleForm';
+import { auth } from '@clerk/nextjs/server';
+
+
+async function FavoriteToggleButton({ productId }: { productId: string }) {
+
+  const {userId} = auth();
+  if(!userId) return <CardSignInButton/>;
+  const favoriteId = await fetchFavoriteId({productId})
+
+
   return (
-    <Button size='icon' variant='outline' className='p-2 cursor-pointer'>
-      <FaHeart />
-    </Button>
+    <FavoriteToggleForm favoriteId={favoriteId} productId={productId} />
   );
 }
 export default FavoriteToggleButton;
