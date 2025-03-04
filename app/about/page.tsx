07509466ff26f846c3c3a16e515f26dd9d28dbd7
Cosmin Sas <cosmindariussas@gmail.com>
2025-03-04 17:59:51 +0000

    added some proper description for hero and about pages

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -7,16 +7,13 @@ function AboutPage() {
   return (
     <section>
       <h1 className='flex flex-wrap gap-2 sm:gap-x-6 items-center justify-center text-4xl font-bold leading-none tracking-wide sm:text-6xl'>
-        We love
+        About
         <span className='bg-primary py-2 px-4 rounded-lg tracking-widest text-white'>
-          store
+          us
         </span>
       </h1>
       <p className='mt-6 text-lg tracking-wide leading-8 max-w-2xl mx-auto text-muted-foreground'>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero hic
-        distinctio ducimus temporibus nobis autem laboriosam repellat, magni
-        fugiat minima excepturi neque, tenetur possimus nihil atque! Culpa nulla
-        labore nam?
+      At our store, we believe in offering more than just products – we offer a personalized shopping experience that reflects creativity, style, and individuality. From unique decor pieces to stylish accessories, every item in our collection is handpicked for its quality and charm. Our goal is to help you find items that bring joy and personality to your everyday life.
       </p>
     </section>
   )
--- components/home/Hero.tsx
+++ components/home/Hero.tsx
@@ -8,10 +8,10 @@ function Hero() {
     <section className='grid grid-cols-1 lg:grid-cols-2 gap-24 items-center'>
       <div>
         <h1 className='max-w-xl font-bold text-4xl tracking-tight sm:text-6xl'>
-          We are changing the way people shop
+        Shopping for Everyone
         </h1>
         <p className='mt-8 max-w-xl text-lg leading-8 text-muted-foreground'>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum itaque porro eius vel a fugiat ipsa deserunt consectetur accusantium laudantium!
+        We’re here to offer a fresh approach to shopping, featuring unique, high-quality products that add a touch of personality to your life. Whether it’s fun decor or stylish accessories, every item is handpicked for its charm and quality. Enjoy a simple, enjoyable shopping experience that’s all about you!
         </p>
         <Button asChild size='lg' className='mt-10'>
           <Link href='/products'>Our products</Link>
